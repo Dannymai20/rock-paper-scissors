7dede5c700e6946ad0e4130a3b32f0a852c0225e
Dannymai20 <dannymai20@yahoo.com>
2022-08-03 12:33:47 -0400

    Added function playRound and game() to play the game

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ function getComputerChoice () {
     let randomizer = Math.floor(Math.random() * 3) + 1;
 
     if(randomizer == 1) {
-        console.log("rock")
+        return "rock"
     } else if(randomizer == 2) {
-        console.log('paper')
+        return "paper"
     } else if (randomizer == 3) {
-        console.log('scissors')
+        return "scissors"
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+
+    if(playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats rock"
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beat paper"
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors"
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats paper"
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors"
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock"
+    } else if (playerSelection.toLowerCase() ==  computerSelection) {
+        return "Tie!"
+    } 
+
+  }
+
+  function game() {
+    for (let i = 0; i < 5; i++) {
+
+        const playerSelection = prompt("Pick rock, paper, or scissors")
+        const computerSelection = getComputerChoice();
+
+        console.log("Player picked " + playerSelection + " and computer picked " + computerSelection)
+        console.log(playRound(playerSelection, computerSelection));
+     }
   }
    
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+game()
